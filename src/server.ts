1a0c3e19c9b10df01a44eda180a29cf92d8b0ced
Andrelino Silva <andrelinodev@gmail.com>
2022-05-07 15:33:39 -0300

    Ajustes backend par deploy

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,20 @@ import { routes } from './routes';
 
 const app = express();
 
+app.use(express.json({ limit: '50mb' }));
+app.use(
+    express.urlencoded({
+        limit: '200mb',
+        extended: true,
+        parameterLimit: 50000,
+    }),
+);
+app.use(express.text({ limit: '200mb' }));
 app.use(cors());
-
-app.use(express.json());
-
 app.use(routes);
 
 app.listen(process.env.SERVER_API_PORT || 3333, () => {
     console.log(
-        '🔥 HTTP server is running on port 4000 🚀 | Feito com 💜 por Andrelino Silva',
+        '🔥 HTTP server is running on port 3333 🚀 | Feito com 💜 por Andrelino Silva',
     );
 });
